f57bcb2cc577f27a8722df0b9e1899ca1ee06343
Steve33566 <colbertnana7@gmail.com>
2025-03-31 12:28:55 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,8 +9,8 @@ def exercice(valeur):
     etoiles = ''
     for i in range(valeur):
         etoiles += '*'
-    return etoiles 
-#comment
+    return etoiles #comm
+
 
 if __name__ == "__main__":
   app.run(debug=True)
