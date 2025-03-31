22ebe8fe89afac987dc6500f171d190484a04c79
Steve33566 <colbertnana7@gmail.com>
2025-03-31 16:59:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/<int:valeur>')
 def exercice(valeur):
     etoiles = ''
-    for j in range(valeur,0,-1):
-        for i in range(j):
+    for j in range(valeur):
+        for i in range(valeur-j):
             etoiles += '*' 
         etoiles += '<br>'
     return etoiles
