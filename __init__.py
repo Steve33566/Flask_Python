8ac3dc0eb99e6c390d25b9ed5ed1b1c94bb1666e
Steve33566 <colbertnana7@gmail.com>
2025-04-07 10:42:21 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,18 +3,19 @@ from flask import render_template
 from flask import json                                                                                                                                     
 app = Flask(__name__)                                                                                                                  
 
-
 @app.route('/<int:valeur>')
 def exercice(valeur):
-   etoiles = ''
-    for j in range(valeur):
-        for i in range(valeur-j):
-            etoiles += '+'   
-        for k in range(j+1):
-            etoiles += '*'
+    etoiles = ''
+    
+    for j in range(1, valeur + 1):
+        etoiles += '&nbsp' * (valeur - j)
+        for i in range(1, j + 1):
+            etoiles += str(i)
+        for i in range(j - 1, 0, -1):
+            etoiles += str(i)
+        
         etoiles += '<br>'
     return etoiles
 
-
-if __name__ == "__main__":
-  app.run(debug=True)
+if _name_ == "__main__":
+    app.run(debug=True)
