b8c3c1721e11d0e4056f9e3f7b7ace83d08691af
Steve33566 <colbertnana7@gmail.com>
2025-04-07 11:03:41 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,12 +4,14 @@ from flask import json
 app = Flask(__name__)                                                                                                                  
 
 
-@app.route('/<int:valeur>')
-def exercice(valeur):
-    etoiles = ''
-     for j in range(1, valeur + 1):
-        etoiles += '&nbsp' * (valeur - j)
-    return etoiles 
+@app.route('/')
+def formulaire():
+    return '''
+    <form action="/suite" method="get">
+        Entrez un nombre : <input type="number" name="valeur" min="2" required>
+        <input type="submit" value="Générer">
+    </form>
+    '''
 
 
 if __name__ == "__main__":
