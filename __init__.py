7436aa2dc6a132b5d19f907e7b6d809b83c5df90
Steve33566 <colbertnana7@gmail.com>
2025-04-07 11:05:54 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from flask import render_template
 from flask import json                                                                                                                                     
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__)
 
+@app.route('/<int:valeur>')
+def exercice(valeur):
+    a, b = 0, 1
+    count = [str(0)] 
 
-@app.route('/')
-def formulaire():
-    return '''
-    <form action="/suite" method="get">
-        Entrez un nombre : <input type="number" name="valeur" min="2" required>
-        <input type="submit" value="Générer">
-    </form>
-    
+    if valeur > 1:
+        count.append(str(b))
+
+    for _ in range(2, valeur):
+        c = a + b
+        count.append(str(c))
+        a, b = b, c
+
+    return ', '.join(count)  
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
