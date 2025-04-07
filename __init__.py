1db9af1cbc3adec95ffa4c888e3d41a26009925e
Steve33566 <colbertnana7@gmail.com>
2025-04-07 15:39:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,11 +11,11 @@ def exercice(valeurs):
     liste_nombres = valeurs.split('/')
     liste_nombres = [int(n) for n in liste_nombres]
 
-    for i in range(len(liste_nombres) + 1):
-        if liste_nombres[i] > liste_nombres[i - 1]:
-            liste_nombres[i - 1] = liste_nombres[i]
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] < liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
 
-    return f"Le nombre minimal est : {liste_nombres[1]}"
+    return f"Le nombre minimum est : {liste_nombres[-1]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
