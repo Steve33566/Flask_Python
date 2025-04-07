ef5e3afda50dc98dd438c7f1c02663da29783f3a
Steve33566 <colbertnana7@gmail.com>
2025-04-07 14:56:59 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,16 +4,18 @@ from flask import json
 
 
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
-    liste_nombres = [int(n) for n in liste_nombres]
-    resultat = 0
-    for n in liste_nombres:
-        resultat = resultat + n
-    return str(resultat)
+    liste_nombres = [int(nombre) for nombre in liste_nombres]
+    valeur_max = liste_nombres[0]
+    for nombre in liste_nombres:
+        if nombre > valeur_max:
+            valeur_max=nombre
+    return str(valeur_max)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
