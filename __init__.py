6dd42de4ff9f34b244ea42eb465dc49d5ed08bc9
Steve33566 <colbertnana7@gmail.com>
2025-04-07 15:19:53 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,14 +9,13 @@ app = Flask(__name__)
 @app.route('/<path:valeurs>')
 def exercice(valeurs):
     liste_nombres = valeurs.split('/')
-    liste_nombres = [int(nombre) for nombre in liste_nombres]
-    valeur_max = liste_nombres[0]
-    for nombre in liste_nombres:
-        if nombre > valeur_max:
-            valeur_max=nombre
-    return str(valeur_max)
+    liste_nombres = [int(n) for n in liste_nombres]
 
+    for i in range(len(liste_nombres) - 1):
+        if liste_nombres[i] > liste_nombres[i + 1]:
+            liste_nombres[i + 1] = liste_nombres[i]
+
+    return f"Le nombre maximum est : {liste_nombres[-1]}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
